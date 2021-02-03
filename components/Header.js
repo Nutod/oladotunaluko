544a173add8ef31ad711c45b6dfbdd4aac60987e
Nutod <alukodotun@gmail.com>
2021-02-04 04:40:43 +0700

    Added new hero component

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import Head from "next/head"
+
+export default function Header() {
+  return (
+    <header class="text-gray-600 body-font ">
+      <div class="container w-7/12 border-b mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
+        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <span class="text-xl">Oladotun Aluko</span>
+        </a>
+        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <a class="mr-5 hover:bg-gray-200 py-1 px-3 focus:outline-none rounded">
+            About
+          </a>
+          <a class="hover:bg-gray-200 py-1 px-3 focus:outline-none rounded">
+            Contact
+          </a>
+        </nav>
+      </div>
+    </header>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,127 @@
-import Head from "next/head"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import Hero from "../components/Hero"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Oladotun Aluko</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Header />
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-4xl font-bold">
-          Site under currently under maintenance
-        </h1>
-      </main>
-    </div>
+      <Hero />
+
+      <section class="text-gray-600 body-font overflow-hidden">
+        <div class="container px-5 py-12 mx-auto w-7/12">
+          <h2 className="title-font sm:text-4xl text-3xl mb-8 font-medium text-gray-900">
+            Blog
+          </h2>
+          <div class="-my-8 divide-y divide-gray-100">
+            <div class="py-8 flex flex-wrap md:flex-nowrap">
+              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span class="font-semibold title-font text-gray-700">
+                  CATEGORY
+                </span>
+                <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+              </div>
+              <div class="md:flex-grow">
+                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
+                  Bitters hashtag waistcoat fashion axe chia unicorn
+                </h2>
+                <p class="leading-relaxed">
+                  Glossier echo park pug, church-key sartorial biodiesel
+                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
+                  moon party messenger bag selfies, poke vaporware kombucha
+                  lumbersexual pork belly polaroid hoodie portland craft beer.
+                </p>
+                <a class="text-indigo-500 inline-flex items-center mt-4">
+                  Learn More
+                  <svg
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div class="py-8 flex flex-wrap md:flex-nowrap">
+              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span class="font-semibold title-font text-gray-700">
+                  CATEGORY
+                </span>
+                <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+              </div>
+              <div class="md:flex-grow">
+                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
+                  Meditation bushwick direct trade taxidermy shaman
+                </h2>
+                <p class="leading-relaxed">
+                  Glossier echo park pug, church-key sartorial biodiesel
+                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
+                  moon party messenger bag selfies, poke vaporware kombucha
+                  lumbersexual pork belly polaroid hoodie portland craft beer.
+                </p>
+                <a class="text-indigo-500 inline-flex items-center mt-4">
+                  Learn More
+                  <svg
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div class="py-8 flex flex-wrap md:flex-nowrap">
+              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span class="font-semibold title-font text-gray-700">
+                  CATEGORY
+                </span>
+                <span class="text-sm text-gray-500">12 Jun 2019</span>
+              </div>
+              <div class="md:flex-grow">
+                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
+                  Woke master cleanse drinking vinegar salvia
+                </h2>
+                <p class="leading-relaxed">
+                  Glossier echo park pug, church-key sartorial biodiesel
+                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
+                  moon party messenger bag selfies, poke vaporware kombucha
+                  lumbersexual pork belly polaroid hoodie portland craft beer.
+                </p>
+                <a class="text-indigo-500 inline-flex items-center mt-4">
+                  Learn More
+                  <svg
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </>
   )
 }
