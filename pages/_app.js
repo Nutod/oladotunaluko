1e405163cef42520eb8aef94bcd949d96fa1bf1c
Nutod <alukodotun@gmail.com>
2021-02-21 22:36:55 +0700

    Updated files and packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import Head from "next/head"
-import "../styles/globals.css"
+import Head from 'next/head'
+import '../styles/main.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600&family=Open+Sans&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=PT+Sans&display=swap"
           rel="stylesheet"
         />
       </Head>
