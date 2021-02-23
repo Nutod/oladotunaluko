bc775601465ce10f61a8deedcc6130052707c2cd
Nutod <alukodotun@gmail.com>
2021-02-24 03:00:57 +0700

    Added new hero section styling

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,9 @@
 export default function Hero() {
   return (
-    <section
-      className="text-gray-500 body-font"
-      style={{ background: 'yellow' }}
-    >
-      <div className="container mx-auto border-b flex px-5 py-24 items-center justify-center flex-col">
-        <div className="lg:w-2/3 w-full">
-          <h5 className="font-brand sm:text-5xl text-6xl mb-4 font-bold text-gray-900">
-            Hi! Welcome to my website 👋
-          </h5>
+    <section className="hero">
+      <div className="container">
+        <div>
+          <h5>Hi! Welcome to my website 👋</h5>
           <p className="hero-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero soluta
             illo a eum, quibusdam unde doloribus fugiat veritatis incidunt vitae
