becf3ffe2f8045456676440ee0d4f8ce4b3be7a2
Nutod <alukodotun@gmail.com>
2021-02-24 03:14:55 +0700

    Added iimage dummy data

--- components/LatestPosts.js
+++ components/LatestPosts.js
@@ -1,23 +1,27 @@
 const posts = [
   {
+    image: 'https://placeimg.com/640/480/nature',
     data: 9 / 8 / 2035,
     title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
     excerpt:
       'man member sign warm mice ear swung learn clothing police higher row slightly tropical discuss near exactly seldom quite west composition dozen come hope',
   },
   {
+    image: 'https://placeimg.com/640/480/tech',
     data: 2 / 6 / 2078,
     title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
     excerpt:
       'sister definition upward given composition strength heard substance difficult half knife children measure making union frighten everyone spend low worth sign shade fifty recognize',
   },
   {
+    image: 'https://placeimg.com/640/480/arch',
     data: 9 / 3 / 2072,
     title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
     excerpt:
       'torn busy number steel tone terrible sail wolf finally generally promised horse dirt noted heading reach week song fed deal original view cut possibly',
   },
   {
+    image: 'https://placeimg.com/640/480/nature',
     data: 5 / 13 / 2049,
     title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
     excerpt:
@@ -32,9 +36,10 @@ export default function LatestPosts() {
       <div className="grid">
         {posts.map(post => (
           <div>
+            <img src={post.image} alt="Blog Image" />
             <h4>{post.title}</h4>
             <p>{post.excerpt}</p>
-            <p>{post.data}</p>
+            <p>{new Date(post.data).toDateString()}</p>
           </div>
         ))}
       </div>
