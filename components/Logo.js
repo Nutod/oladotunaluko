d3243d054619df09f78b932000ccc359d33c9a49
Nutod <alukodotun@gmail.com>
2021-02-23 19:47:37 +0700

    Adjusted footer styles

--- components/Logo.js
+++ components/Logo.js
@@ -2,14 +2,14 @@ import Link from 'next/link'
 
 export default function Logo() {
   return (
-    <div>
-      <h3 className="no-margin">
+    <div className="logo">
+      <h4 className="no-margin">
         <Link href="/">
           <a>
             oladotun<span>aluko</span>
           </a>
         </Link>
-      </h3>
+      </h4>
     </div>
   )
 }
