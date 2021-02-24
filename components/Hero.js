e58146fb14096ebbae0100dcf5a0bad9837e8812
Nutod <alukodotun@gmail.com>
2021-02-24 07:21:27 +0700

    Added new card option

--- components/Hero.js
+++ components/Hero.js
@@ -3,12 +3,17 @@ export default function Hero() {
     <section className="hero">
       <div className="container">
         <div>
-          <h5>Hi! Welcome to my website 👋</h5>
+          <h5>Hi! My name is Oladotun</h5>
           <p className="hero-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero soluta
             illo a eum, quibusdam unde doloribus fugiat veritatis incidunt vitae
             totam <a href="">link</a> nam cumque explicabo facere. Commodi nobis
             harum corporis temporibus.
+          </p>
+          <p className="hero-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit tempore eaque hic consectetur atque iusto, pariatur
+            modi perferendis nobis officiis.
           </p>
         </div>
       </div>
