3256daecb2086b2e491556d50adc51f4874a9838
Nutod <alukodotun@gmail.com>
2021-02-25 07:08:54 +0700

    Updated theme context

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -1,32 +1,32 @@
 import React from 'react'
 
 function getInitialColorMode() {
-  const persistedColorPreference = window.localStorage.getItem('color-mode')
-  const hasPersistedPreference = typeof persistedColorPreference === 'string'
+  if (typeof window !== 'undefined') {
+    const persistedColorPreference = window.localStorage.getItem('color-mode')
+    const hasPersistedPreference = typeof persistedColorPreference === 'string'
 
-  if (hasPersistedPreference) {
-    return persistedColorPreference
+    if (hasPersistedPreference) {
+      return persistedColorPreference
+    }
+
+    const mql = window.matchMedia('(prefers-color-scheme: dark)')
+    const hasMediaQueryPreference = typeof mql.matches === 'boolean'
+
+    if (hasMediaQueryPreference) {
+      return mql.matches ? 'dark' : 'light'
+    }
+
+    return 'light'
+  } else {
+    return 'light'
   }
-
-  const mql = window.matchMedia('(prefers-color-scheme: dark)')
-  const hasMediaQueryPreference = typeof mql.matches === 'boolean'
-
-  if (hasMediaQueryPreference) {
-    return mql.matches ? 'dark' : 'light'
-  }
-
-  return 'light'
 }
 
 const ThemeContext = React.createContext()
 ThemeContext.displayName = 'ThemeContext'
 
 export default function ThemeProvider({ children }) {
-  const [theme, setTheme] = React.useState('light')
-
-  React.useLayoutEffect(() => {
-    setTheme(getInitialColorMode())
-  }, [])
+  const [theme, setTheme] = React.useState(() => getInitialColorMode())
 
   const toggleTheme = () => {
     setTheme(theme => (theme === 'light' ? 'dark' : 'light'))
