6b05d5eb5459f897b3fa5172ef44a6a326eacc24
Nutod <alukodotun@gmail.com>
2021-02-26 07:38:59 +0700

    Added new 404 page

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,9 @@
-import React from 'react'
-
-import Head from 'next/head'
 import ThemeProvider from '../context/ThemeContext'
 import '../styles/main.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital@0;1&family=Noto+Sans&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preload" href="/fonts/Gilroy-Bold.ttf" as="font" />
-      </Head>
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
