af65d39d744633d0a09574c24c24579ee128f36a
Nutod <alukodotun@gmail.com>
2021-02-26 22:53:24 +0700

    Changed ThemeContext implementation

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -1,34 +1,13 @@
 import React from 'react'
 
-// TODO: Fix the context issue with the theming
-function getInitialColorMode() {
-  if (typeof window !== 'undefined') {
-    const persistedColorPreference = window.localStorage.getItem('color-mode')
-    const hasPersistedPreference = typeof persistedColorPreference === 'string'
-
-    if (hasPersistedPreference) {
-      return persistedColorPreference
-    }
-
-    return 'light'
-  } else {
-    return 'light'
-  }
-}
-
 const ThemeContext = React.createContext()
 ThemeContext.displayName = 'ThemeContext'
 
 export default function ThemeProvider({ children }) {
-  const [theme, setTheme] = React.useState(() => getInitialColorMode())
+  const [theme, setTheme] = React.useState('light')
 
   const toggleTheme = () => {
-    let updatedThemeValue = theme === 'light' ? 'dark' : 'light'
-    setTheme(updatedThemeValue)
-
-    if (typeof window !== 'undefined') {
-      window.localStorage.setItem('color-mode', updatedThemeValue)
-    }
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'))
   }
 
   const value = { theme, toggleTheme }
