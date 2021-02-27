0f1139322b191a6da40bdc806a700d3b9f0d23d9
Nutod <alukodotun@gmail.com>
2021-02-27 18:01:19 +0700

    Added new about content

--- components/About.js
+++ components/About.js
@@ -1,27 +1,45 @@
 export default function About() {
   return (
     <div className="about-content">
-      <h3>Hi! My name is Oladotun 👋</h3>
+      <h3>Hi there 👋🏿</h3>
+      <h4>I see you're here trying to find out more about me 😃</h4>
+      <p>Read on 👇🏿</p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-        perspiciatis cumque fugiat fuga a voluptate tempore, facilis dolores
-        voluptates sit debitis? Magnam, ea cum. Ipsam necessitatibus minus
-        doloribus ratione alias cupiditate sed est facilis adipisci eum! Iste
-        accusantium fugiat facilis.
+        My name is Oladotun. I'm Nigerian 🇳🇬. I was born the same year the
+        beautiful people over at Netscape Communications in response to the
+        increase in demand of client side scripting languages increased decided
+        the web needed a language of it's own - that's{' '}
+        <strong>Javascript</strong> in case you didn't know.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-        perspiciatis cumque fugiat fuga a voluptate tempore, facilis dolores
-        voluptates sit debitis? Magnam, ea cum. Ipsam necessitatibus minus
-        doloribus ratione alias cupiditate sed est facilis adipisci eum! Iste
-        accusantium fugiat facilis.
+        In 2015, I was offered and accepted a full-time opportunity with{' '}
+        <a
+          rel="noopener"
+          href="https://www.iqubesolutions.com.ng/"
+          target="_blank"
+        >
+          iQube Labs
+        </a>
+        , as well as continuing to work as an intern up until graduation. Since
+        my University graduation in 2017, I've gone on to write code for several
+        companies including{' '}
+        <a rel="noopener" href="https://kudi.com/" target="_blank">
+          KudiAI
+        </a>{' '}
+        and{' '}
+        <a rel="noopener" href="https://www.sprinble.com/" target="_blank">
+          Sprinble LLC
+        </a>
+        .
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure fuga
-        consectetur, expedita unde ullam vero tenetur, in iusto vel aliquam
-        dicta aut eius numquam? Illum fugiat culpa beatae libero, quod esse,
-        delectus quo, maxime modi laudantium aspernatur dicta voluptatem
-        dolores?
+        If you have an exciting project and you'd like to get in touch with me,
+        I'm always an <a href="mailto:alukodotun@gmail.com">email</a> away and
+        if you prefer, you could always send a DM on{' '}
+        <a rel="noopener" href="https://twitter.com/Nutod_20">
+          twitter
+        </a>
+        .
       </p>
     </div>
   )
