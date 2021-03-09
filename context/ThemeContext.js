f0097d3d6954f96caaa59eadde2cfd7b928f04de
Nutod <alukodotun@gmail.com>
2021-03-09 23:48:10 +0700

    Updated theme context value

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -7,15 +7,28 @@ export default function ThemeProvider({ children }) {
   const [mounted, setMounted] = React.useState(false)
   const [theme, setTheme] = React.useState('light')
 
-  React.useEffect(() => setMounted(true), [])
+  React.useLayoutEffect(() => setMounted(true), [])
 
-  console.log(mounted)
+  React.useEffect(() => {
+    // 1. Check localstorage
+    const persistedThemeValue = localStorage.getItem('--color-mode')
+    console.log(persistedThemeValue)
+    if (persistedThemeValue) {
+      setTheme(persistedThemeValue)
+      return
+    }
+
+    // 2. Check prefers color scheme
+
+    // 3. return a default value as third option
+    setTheme('light')
+  }, [])
 
   const toggleTheme = () => {
     setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'))
   }
 
-  const value = { theme, toggleTheme }
+  const value = { theme, toggleTheme, mounted }
 
   return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
 }
