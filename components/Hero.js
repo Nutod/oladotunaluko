51f20e435e44884dd357087d534c0d54257b3637
Oladotun Aluko <alukodotun@gmail.com>
2021-12-09 18:19:41 +0100

    Update Hero

--- components/Hero.js
+++ components/Hero.js
@@ -8,11 +8,7 @@ export default function Hero() {
           <h5>Hi! My name is Oladotun</h5>
           <p className="hero-text">
             I'm a Software Developer focused on the front-end things of the web.
-            This is my site where I write about things I'm interested in. If
-            you'd like to know more about me, look{' '}
-            <Link href="/about">
-              <a>here</a>
-            </Link>
+            This is my site where I write about things I'm interested in.
             .
           </p>
           <p className="hero-text">
